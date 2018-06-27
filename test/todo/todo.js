f412fd5dc50cf8a4f0f183477111ea5c78ca8223
takayuki.kayawari <kayamelo@gmail.com>
2018-06-27 23:29:16 +0900

    task追加のメソッドを追加

--- test/todo/todo.js
+++ test/todo/todo.js
@@ -5,6 +5,17 @@ var vm = new Vue({
             {id: 1, name: 'hoge'},
             {id: 2, name: 'fuga'},
             {id: 3, name: 'hogefuga'}
-        ]
+        ],
+        newTask: ''
+    },
+    methods: {
+        createTask: function(event){
+            var id = this.tasks.length + 1;
+            this.tasks.push({
+                id: id,
+                name: this.newTask
+            });
+            this.newTask = '';
+        }
     }
 })
