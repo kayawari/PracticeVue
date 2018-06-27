a307e322765d39216a62f33a49d9ce00627caf21
takayuki.kayawari <kayamelo@gmail.com>
2018-06-27 23:12:17 +0900

    sample todo app

---
+++ test/todo/todo.js
@@ -0,0 +1,10 @@
+var vm = new Vue({
+    el: '#tasks-index'
+    data: {
+        tasks: [
+            {id: 1, name: 'hoge'},
+            {id: 2, name: 'fuga'},
+            {id: 3, name: 'hogefuga'}
+        ]
+    }
+})
